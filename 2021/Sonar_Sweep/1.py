09562911a92abb3280f82abd94726ec11f254af8
Gabriel <gviera2442@gmail.com>
2021-12-19 17:06:51 -0400

    First Day Push

--- 2021/Sonar_Sweep/1.py
+++ 2021/Sonar_Sweep/1.py
@@ -1,12 +1,24 @@
-def Sonar_Sweep(file):
+def Part_A(file):
     fopen = open(file,'r')
+    fhand = fopen.readlines()
     count = 0
-    previous_item = None
-    for line in fopen:
-        line = int(line)
-
-        if previous_item != None and line > previous_item:
+    
+    for i in range(len(fhand)-1):
+        if int(fhand[i]) < int(fhand[i+1]):
             count += 1
-
-        previous_item = line
     return count
+    
+
+def Part_B(file):
+    fopen = open(file,'r')
+    fhand = fopen.readlines()
+    count = 0
+    
+    for i in range(0,len(fhand)):
+        previ_item = int(fhand[i-1]) + int(fhand[i-2])+ int(fhand[i-3])
+        next_item = int(fhand[i-1]) + int(fhand[i-2]) + int(fhand[i])
+        if next_item > previ_item: count += 1
+    
+    return count
+
+print(Part_B(r"AdventOfCode\2021\Sonar_Sweep\input.txt"))
